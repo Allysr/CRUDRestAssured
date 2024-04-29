7d42562057f6d6772c2bb0d8219e39434290f88c
Alicia <alicia.rsouza@gmail.com>
2024-04-29 15:14:44 -0300

    docs: adição de mais uma rota

--- src/test/java/testes/BaseTest.java
+++ src/test/java/testes/BaseTest.java
@@ -2,7 +2,7 @@ package testes;
 
 import io.restassured.RestAssured;
 
-public class BaseTest {
+public class BaseTest  {
 
     public static void pegarRota() {
         RestAssured.baseURI = "https://teste-deploy-d69a89680fb9.herokuapp.com/api";
