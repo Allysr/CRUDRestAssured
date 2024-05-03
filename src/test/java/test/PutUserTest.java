83441daceceeab1db08c45857a63c2d0d2c298e3
allys <alicia.rsouza@gmail.com>
2024-05-02 21:46:45 -0300

    test: adicionando teste put

---
+++ src/test/java/test/PutUserTest.java
@@ -0,0 +1,106 @@
+package test;
+
+import factory.Usuario.UsuarioData;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+
+
+public class PutUserTest extends BaseTest {
+
+    @Test
+    @DisplayName("Deve validar status code 200 ao atualizar usuário com sucesso")
+    public void deveValidarUsuarioEStatusCode200() {
+        UsuarioData usuario = new UsuarioData();
+
+        Integer id = RestAssured
+                .given()
+                .contentType("application/json")
+                .body(usuario.novoUsuario())
+                .when().post("/users")
+                .then()
+                .statusCode(201)
+                .extract()
+                .path("id");
+
+
+        RestAssured
+                .given()
+                .contentType("application/json")
+                .body(usuario.novoUsuario())
+                .when().put("/users/" + id)
+                .then()
+                .statusCode(200);
+    }
+
+
+    @Test
+    @DisplayName("Deve validar status code 400 ao enviar CPF inválido")
+    public void deveValidarStatus400AoEnviarCPFInvalido() {
+        UsuarioData usuario = new UsuarioData();
+
+        Integer id = RestAssured
+                .given()
+                .contentType("application/json")
+                .body(usuario.novoUsuario())
+                .when().post("/users")
+                .then()
+                .statusCode(201)
+                .extract()
+                .path("id");
+
+        RestAssured
+                .given()
+                .contentType("application/json")
+                .body(usuario.usuarioCPFInvalido())
+                .when().put("/users/" + id)
+                .then()
+                .statusCode(400)
+                .body("mensagem", equalTo("CPF invalido"));
+    }
+
+    @Test
+    @DisplayName("Deve validar status code 400 ao tentar atualizar CPF existente")
+    public void deveValidarStatus400AoAtualizarCPFExistente() {
+        UsuarioData usuario = new UsuarioData();
+
+        Integer id = RestAssured
+                .given()
+                .contentType("application/json")
+                .body(usuario.novoUsuario())
+                .when().post("/users")
+                .then()
+                .statusCode(201)
+                .extract()
+                .path("id");
+
+        RestAssured
+                .given()
+                .contentType("application/json")
+                .body(usuario.usuarioCPFExistente())
+                .when().put("/users/" + id)
+                .then()
+                .statusCode(400)
+                .body("mensagem", equalTo("CPF já cadastrado"));
+   }
+
+
+    @Test
+    @DisplayName("Deve validar status code 404 ao tentar atualizar usuário inexistente")
+    public void deveValidarStatusCode404AoTentarAtualizarUsuarioInexistente() {
+        UsuarioData usuario = new UsuarioData();
+
+        RestAssured
+                .given()
+                .contentType("application/json")
+                .body(usuario.novoUsuario())
+                .when().put("/users/7000")
+                .then()
+                .statusCode(404)
+                .body("mensagem", containsString("ID do usuário não encontrado"));
+    }
+
+    }
